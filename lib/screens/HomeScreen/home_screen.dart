405d84d291108f1edb7c75a39180dc28f299d7c3
= <=>
2022-10-01 18:29:06 +0530

    home page ready

--- lib/screens/HomeScreen/home_screen.dart
+++ lib/screens/HomeScreen/home_screen.dart
@@ -7,21 +7,59 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Colors.black,
         body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              Container(
-                color: Colors.red,
-                height: 100,
-                width: double.infinity,
-                child: Center(child: Text('Telescope Parts')),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 96, 99, 100),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                ),
+                child: Center(
+                  child: Container(
+                    height: 200,
+                  ),
+                ),
               ),
-              Text("Games:"),
-              RoundedGameButton(),
-              RoundedGameButton(),
-              RoundedGameButton(),
-              RoundedGameButton(),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 20),
+                height: 2,
+                width: double.infinity,
+                color: Colors.white,
+              ),
+              Center(
+                child: Text(
+                  "Games",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              RoundedGameButton(
+                name: "Name 1",
+                desc: "Description of Game 1",
+                onPressed: () {},
+              ),
+              RoundedGameButton(
+                name: "Name 2",
+                desc: "Description of Game 1",
+                onPressed: () {},
+              ),
+              RoundedGameButton(
+                name: "Name 3",
+                desc: "Description of Game 1",
+                onPressed: () {},
+              ),
             ],
           ),
         ),
@@ -31,27 +69,57 @@ class HomeScreen extends StatelessWidget {
 }
 
 class RoundedGameButton extends StatelessWidget {
-  const RoundedGameButton({super.key});
+  final String name;
+  final String desc;
+  final Function onPressed;
+  const RoundedGameButton({
+    super.key,
+    required this.name,
+    required this.desc,
+    required this.onPressed,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: TextButton(
-        onPressed: () {},
+        onPressed: () {
+          return;
+        },
         style: TextButton.styleFrom(
+          backgroundColor: Color.fromARGB(255, 230, 226, 226),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(18.0),
           ),
-          backgroundColor: Colors.blue,
         ),
         child: Container(
-          height: 50,
           width: double.infinity,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
+          padding: EdgeInsets.all(8),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  desc,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ],
+            ),
           ),
-          child: Text('1'),
         ),
       ),
     );
