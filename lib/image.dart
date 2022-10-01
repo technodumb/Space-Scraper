2e5776ede34ae8f2955b478d463adb4c654fe70b
abhinav-18max <abhinav200218@gmail.com>
2022-10-01 23:06:53 +0530

    gallery complete

---
+++ lib/image.dart
@@ -0,0 +1,32 @@
+import "package:flutter/material.dart";
+
+class Imageblur extends StatelessWidget {
+ 
+  final List<Image> myImages = List.generate(25, (index) => Image.asset('assets/SpacePics/${index + 1}_pic.jpg', fit: BoxFit.cover));
+
+  final List<Padding> myImg = List.generate(
+    25,
+    (index) => Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Image(
+        image: AssetImage('assets/SpacePics/${index + 1}_pic.jpg'),
+        fit: BoxFit.cover,
+      ),
+    ),
+  );
+  Imageblur({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Gallery"),
+        centerTitle: true,
+      ),
+      body: GridView.count(
+        crossAxisCount: 3,
+        children: [...myImg],
+      ),
+    );
+  }
+}
